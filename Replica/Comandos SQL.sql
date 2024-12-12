34777bb9d13a77602e9541e73550b12fe71d4311
Silvia <veronica-14-1998@hotmail.com>
2024-12-12 15:59:12 -0500

    Agregar configuración de replicación en MySQL con múltiples nodos en Docker Compose

--- Replica/Comandos SQL.sql
+++ Replica/Comandos SQL.sql
@@ -4,7 +4,8 @@ SHOW MASTER STATUS;
 -- Verificar el estado de la replicación
 SHOW SLAVE STATUS;
 -- Cambiar el master host, user, password, log file y log pos
-CHANGE MASTER TO MASTER_HOST='', MASTER_USER='',MASTER_PASSWORD='',
+CHANGE MASTER TO MASTER_HOST='', MASTER_USER='',MASTER_PASSWORD='',--Este es para configurar el esclavo
+
 CHANGE MASTER TO MASTER_LOG_FILE='mysql-bin.000003', MASTER_LOG_POS=477;
 
 
