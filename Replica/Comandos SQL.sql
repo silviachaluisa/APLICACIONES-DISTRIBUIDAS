2454212d01e21edc766ff8a412de72dc4167e7ae
Silvia <veronica-14-1998@hotmail.com>
2024-12-18 21:35:14 -0500

    Agregar configuración de replicación en MySQL para múltiples nodos y actualizar docker-compose

--- Replica/Comandos SQL.sql
+++ Replica/Comandos SQL.sql
@@ -12,3 +12,25 @@ CHANGE MASTER TO MASTER_LOG_FILE='mysql-bin.000003', MASTER_LOG_POS=477;
 -- Iniciar la replicación
 START SLAVE;
 
+--Creación de usuarios para otorgar permisos
+CREATE USER 'repl_user'@'%' IDENTIFIED BY 'repl_password';
+GRANT REPLICATION SLAVE ON *.* TO 'repl_user'@'%';
+FLUSH PRIVILEGES;
+
+--Configurar nodo maestro 1
+CHANGE MASTER TO
+    MASTER_HOST='mysql-master-2',
+    MASTER_USER='repl_user',
+    MASTER_PASSWORD='repl_password',
+    MASTER_LOG_FILE='[File_de_master2]',
+    MASTER_LOG_POS=[Position_de_master2];
+START SLAVE;
+
+--Configurar nodo maestro 2
+CHANGE MASTER TO
+    MASTER_HOST='mysql-master-1',
+    MASTER_USER='repl_user',
+    MASTER_PASSWORD='repl_password',
+    MASTER_LOG_FILE='[File_de_master1]',
+    MASTER_LOG_POS=[Position_de_master1];
+START SLAVE;
