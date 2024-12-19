949445a5606c580ac5fc47a7c5c3343dc1e87817
Silvia <veronica-14-1998@hotmail.com>
2024-12-19 14:51:30 -0500

    Agregar configuración de servidores web en Python y balanceador de carga con Nginx

---
+++ Balance/Servidor3/servidor3.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+# Crear un servidor web
+servidor = Flask(__name__)
+
+# Crear una ruta
+@servidor.route('/')
+def hola():
+    return "Hola desde servidor 3"
+
+if __name__ == '__main__':
+    servidor.run(
+        host='0.0.0.0', # Especificar la IP para que sea visible desde cualquier lugar
+        port=5000 # Especificar el puerto
+    )
