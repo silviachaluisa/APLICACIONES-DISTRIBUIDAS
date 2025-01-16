714425b3bf164524621d0acf3177f304cb147d19
Silvia <veronica-14-1998@hotmail.com>
2025-01-16 16:00:19 -0500

    Agregar pruebas de rendimiento con Locust y configurar Docker Compose para la aplicación

--- Aplicacion1/locustfile.py
+++ Aplicacion1/locustfile.py
@@ -2,7 +2,16 @@ from locust import HttpUser, task, between
 
 class MyUser(HttpUser): #Esta clase hereda de HttpUser, que se utiliza para pruebas de rendimiento HTTP
     wait_time= between(1,2) #Esto establece un tiempo de espera aleatorio entre 1 y 2 segundos entre tareas.
+    host = "http://aplicacion_python:5000" #Establece el host al que se enviarán las solicitudes.
     
     @task
     def index(self): #El método index envía una solicitud GET a la URL especificada.
-        response = self.client.get("http://aplicacion:5000/")
+        response = self.client.get("/")
+    
+    @task
+    def submit(self): #El método submit envía una solicitud POST a la URL especificada.
+        response = self.client.post("/", {
+            "name":"locust",
+            "email":"locust@locust.com",
+            "message":"locust"
+        })
